1f50f1963a196cdebc36f0b243cf64fc28b44a45
dangphank4 <phanlehaidanghsht@gmail,com>
2025-06-21 16:00:43 +0700

    2048

---
+++ STM32CubeIDE/Debug/Application/User/gui/Model.d
@@ -0,0 +1,6 @@
+Application/User/gui/Model.o: \
+ D:/2048_newgame/TouchGFX/gui/src/model/Model.cpp \
+ ../../TouchGFX/gui/include/gui/model/Model.hpp \
+ ../../TouchGFX/gui/include/gui/model/ModelListener.hpp
+../../TouchGFX/gui/include/gui/model/Model.hpp:
+../../TouchGFX/gui/include/gui/model/ModelListener.hpp:
---
+++ STM32CubeIDE/Debug/Drivers/BSP/Components/ili9341.d
@@ -0,0 +1,6 @@
+Drivers/BSP/Components/ili9341.o: \
+ D:/2048_newgame/Drivers/BSP/Components/ili9341/ili9341.c \
+ D:/2048_newgame/Drivers/BSP/Components/ili9341/ili9341.h \
+ D:/2048_newgame/Drivers/BSP/Components/ili9341/../Common/lcd.h
+D:/2048_newgame/Drivers/BSP/Components/ili9341/ili9341.h:
+D:/2048_newgame/Drivers/BSP/Components/ili9341/../Common/lcd.h:
